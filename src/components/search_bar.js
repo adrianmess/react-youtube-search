d69447d03d5fd49417ee1f316da2fe8515edd851
Adrian Messado <adrian.messado@gmail.com>
2018-07-16 11:42:29 -0700

    remove test

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,7 +17,6 @@ class SearchBar extends Component {
         `}
         </style>
       <input onChange={event => this.setState({ searchterm: event.target.value })} />
-      <p><span className="value">Value of the input:</span> {this.state.searchterm}</p>
       </div>
     );
   }
